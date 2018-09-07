63b0ec4aba5c2c82b876ea5eb80fae5e7ce3752a
Masahiro Hayashi <mhayashi@mshackers.co.jp>
2018-09-07 16:37:10 +0900

    Google  Calendar APIにアクセス

--- Program.cs
+++ Program.cs
@@ -1,4 +1,12 @@
 ﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
 
 namespace csharp_googleapi
 {
@@ -6,7 +14,72 @@ namespace csharp_googleapi
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            new Program().Run().Wait();
+        }
+
+        private ICredential GetServiceAccountCredential(string[] scopes)
+        {
+            using (var stream = new FileStream("service_account_key.json", FileMode.Open, FileAccess.Read))
+            {
+                return GoogleCredential.FromStream(stream)
+                     .CreateScoped(scopes).UnderlyingCredential;
+            }
+        }
+
+        private Task<UserCredential> GetUserCredential(string[] scopes)
+        {
+            using (var stream = new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = "token";
+                return GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    scopes,
+                    "user", CancellationToken.None, new FileDataStore(credPath, true));
+            }
+        }
+
+        private async Task Run()
+        {
+            var scopes = new[] { CalendarService.Scope.CalendarReadonly };
+            // ICredential credential = GetServiceAccountCredential(scopes);
+            ICredential credential = await GetUserCredential(scopes);
+            GetCalendar(credential, "(カレンダーID)"); // アクセスしたいカレンダーのID
+        }
+
+        private void GetCalendar(ICredential credential, string calendarId)
+        {
+            var service = new CalendarService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "Get Calendar Sample",
+            });
+
+            EventsResource.ListRequest request = service.Events.List(calendarId);
+            request.TimeMin = DateTime.Now;
+            request.ShowDeleted = false;
+            request.SingleEvents = true;
+            request.MaxResults = 10;
+            request.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime;
+
+            // List events.
+            Events events = request.Execute();
+            Console.WriteLine("Upcoming events:");
+            if (events.Items != null && events.Items.Count > 0)
+            {
+                foreach (var eventItem in events.Items)
+                {
+                    string when = eventItem.Start.DateTime.ToString();
+                    if (String.IsNullOrEmpty(when))
+                    {
+                        when = eventItem.Start.Date;
+                    }
+                    Console.WriteLine("{0} ({1})", eventItem.Summary, when);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No upcoming events found.");
+            }
         }
     }
 }
